86623a18f43199b64de816cff6eb097b5237c364
aserrahima <aserrahima@uoc.edu>
2025-12-23 15:22:56 +0100

    3. dades

---
+++ js/dades.js
@@ -0,0 +1,6 @@
+export const POBLACIONES = [
+  { nombre: "Barcelona", cp: "08001" },
+  { nombre: "Girona", cp: "17001" },
+  { nombre: "Tarragona", cp: "43001" },
+  { nombre: "Lleida", cp: "25001" }
+];
